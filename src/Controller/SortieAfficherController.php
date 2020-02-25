81266a649e98eaef565a65fb36b6d9ba0eb5ea59
Jeremy APPRIOU <jeremy.appriou2019@campus-eni.fr>
2020-02-25 11:10:33 +0100

    methode pour simuler une connexion

--- src/Controller/SortieAfficherController.php
+++ src/Controller/SortieAfficherController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use App\Entity\Sortie;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 /**
  * Class SortieAfficherController
@@ -20,8 +22,15 @@ class SortieAfficherController extends AbstractController
      */
     public function index(Request $request, EntityManagerInterface $entityManager)
     {
+        $this->fakeConnect(1);
         $sortiesRepo = $this->getDoctrine()->getRepository(Sortie::class);
         $res = $sortiesRepo->findAll();
         return $this->render('sortie_afficher/index.html.twig', ['sorties' => $res]);
+    }
+
+    private function fakeConnect(int $id) {
+        $user = $this->getDoctrine()->getRepository(Participant::class)->find(['id' => $id]);
+        $usertoken = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+        $this->get('security.token_storage')->setToken($usertoken);
     }
 }
