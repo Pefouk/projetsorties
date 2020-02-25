06d79c344109eb683eb53c302867b964390689a9
Jeremy APPRIOU <jeremy.appriou2019@campus-eni.fr>
2020-02-25 16:20:54 +0100

    Navbar quasi finie et footer fini

--- src/Form/FiltrerSortiesType.php
+++ src/Form/FiltrerSortiesType.php
@@ -6,6 +6,7 @@ use App\Entity\Campus;
 use App\Entity\Sortie;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -30,7 +31,22 @@ class FiltrerSortiesType extends AbstractType
             ])->add('datemin', DateType::class, [
                 'label' => 'Entre',
                 'required' => false
-            ])//->add()
+            ])->add('datemax', DateType::class, [
+                'label' => 'Et',
+                'required' => false
+            ])->add('organise', CheckboxType::class, [
+                'required'=>false,
+                'label' => 'Sorties dont je suis l\'organisateur/trice'
+            ])->add('inscrit', CheckboxType::class, [
+                'required'=>false,
+                'label' => 'Sorties dont je suis inscrit/e'
+            ])->add('nonInscrit', CheckboxType::class, [
+                'required'=>false,
+                'label' => 'Sorties dont je ne suis pas inscrit/e'
+            ])->add('passee', CheckboxType::class, [
+                'required'=>false,
+                'label' => 'Sorties passées'
+            ])
         ;
     }
 
